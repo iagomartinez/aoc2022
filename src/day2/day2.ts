b4da27914ba992ea3bce4b0d1e46a62fb7fc4c8f
Iago Martínez <iago@fundapps.co>
2022-12-02 12:35:44 +0000

    Score response moves

--- src/day2/day2.ts
+++ src/day2/day2.ts
@@ -1,5 +1,14 @@
 import { readFileSync } from 'fs';
 
 export function score(play:string, response:string): number | undefined {
-    return undefined;
+    switch(response){
+        case 'X':
+            return 1;
+        case 'Y':
+            return 2;
+        case 'Z':
+            return 3;
+        default:
+            return undefined;
+    }    
 }
--- tests/day2.spec.ts
+++ tests/day2.spec.ts
@@ -3,5 +3,18 @@ import {score} from '../src/day2/day2';
 describe('Day 2 score function', () => {
         it ('should return undefined', async () => {
             expect(score('A', 'B')).toBe(undefined);
+        });
+
+        it ('scores X(Rock) as 1', async () => {
+            expect(score('_', 'X')).toBe(1);
         })
+
+        it ('scores Y(Paper) as 2', async () => {
+            expect(score('_', 'Y')).toBe(2);
+        })
+
+        it ('scores Z(Scissors) as 3', async () => {
+            expect(score('_', 'Z')).toBe(3);
+        })
+
     })
