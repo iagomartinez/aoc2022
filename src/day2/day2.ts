0c1767736d8f483685bcd1b65e0fdae42565ba48
Iago Martínez <iago@fundapps.co>
2022-12-02 19:20:07 +0000

    Day 2 ⭐ - code's a bit rough

--- src/day2/day2.ts
+++ src/day2/day2.ts
@@ -1,14 +1,58 @@
 import { readFileSync } from 'fs';
 
-export function score(play:string, response:string): number | undefined {
-    switch(response){
-        case 'X':
-            return 1;
-        case 'Y':
-            return 2;
-        case 'Z':
-            return 3;
+export function score(play:string, response:string): [number, number]{
+    let score : [number, number]= [0,0]
+
+    let playMap = new Map<string, string>([
+        ['X', 'A'],
+        ['Y', 'B'],
+        ['Z', 'C']
+    ]);    
+
+    let mappedResponse:string|undefined = playMap.get(response);
+
+    switch(mappedResponse){
+        case 'A':
+            score[0] = 1;
+            break;
+        case 'B':
+            score[0] = 2;
+            break;
+        case 'C':
+            score[0] = 3;
+            break;
         default:
-            return undefined;
-    }    
+            //return [0, 0];
+            break;
+    }
+
+    if (play === mappedResponse){
+        score[1] = 3;
+    }
+    else if (play === 'A' && mappedResponse === 'B' || play === 'B' && mappedResponse === 'C' || play === 'C' && mappedResponse === 'A' ){
+        score[1] = 6;
+    }
+    else{
+        score[1] = 0;
+    }
+
+    console.log(`${play}, ${response}:${mappedResponse} => ${score}`)
+    return score;
 }
+
+export function parseinput(inputfile : string): string[][] {
+    return readFileSync(inputfile, 'utf-8')
+        .split(/\r?\n/)
+        .filter(line=>line.trim())
+        .map(line => line.trim().split(" "));
+}
+
+export function p1 (inputfile : string): number[] {
+    let parsed = parseinput(inputfile);
+    console.log(parsed);
+    return parsed
+        .map(move => score(move[0], move[1]))
+        .map(score => score[0]+score[1]);    
+}
+
+console.log(`AOC 2022 Day 2 result ⭐: ${ p1('src/day2/input.txt').reduce((acc,val) => acc += val, 0)}`);
--- tests/day2.spec.ts
+++ tests/day2.spec.ts
@@ -1,20 +1,30 @@
-import {score} from '../src/day2/day2';
+import {score, p1, parseinput} from '../src/day2/day2';
 
 describe('Day 2 score function', () => {
-        it ('should return undefined', async () => {
-            expect(score('A', 'B')).toBe(undefined);
+        test.each`
+            play    | response  | result
+            ${'A'}  | ${'X'}    | ${[1,3]}
+            ${'B'}  | ${'Y'}    | ${[2,3]}
+            ${'C'}  | ${'Z'}    | ${[3,3]}
+            ${'A'}  | ${'Y'}    | ${[2,6]}
+            ${'B'}  | ${'Z'}    | ${[3,6]}
+            ${'C'}  | ${'X'}    | ${[1,6]}
+            ${'A'}  | ${'Z'}    | ${[3,0]}
+            ${'B'}  | ${'X'}    | ${[1,0]}
+            ${'C'}  | ${'Y'}    | ${[2,0]}
+            `('calculate($play, $response) should return $result', ({ play, response, result }) => {
+                expect(score(play, response)).toStrictEqual(result);
+            });
+
+        it ('should parse three rows from test input', () => {
+            let parsed = parseinput('src/day2/testinput.txt');
+            console.log(parsed);
+            expect(parsed).toStrictEqual([['A','Y'],['B','X'],['C','Z']])
         });
 
-        it ('scores X(Rock) as 1', async () => {
-            expect(score('_', 'X')).toBe(1);
-        })
-
-        it ('scores Y(Paper) as 2', async () => {
-            expect(score('_', 'Y')).toBe(2);
-        })
-
-        it ('scores Z(Scissors) as 3', async () => {
-            expect(score('_', 'Z')).toBe(3);
-        })
-
-    })
+        it('should work out a score of 15 with test input', () =>{
+            let scores = p1('src/day2/testinput.txt');
+            console.log(scores);
+            expect(scores).toStrictEqual([8,1,6]);
+        });
+    });
