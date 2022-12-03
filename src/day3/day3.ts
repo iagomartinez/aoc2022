6158415e8567bd28825838569889854043fe3eba
Iago Martínez <iago@fundapps.co>
2022-12-03 23:11:16 +0000

    Day 3 ⭐ :sparkles:

--- src/day3/day3.ts
+++ src/day3/day3.ts
@@ -1,5 +1,11 @@
 import { readFileSync } from 'fs';
 
+const alphabet = 'abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ'
+
+export function prioritise(item: string): number{
+    return alphabet.indexOf(item) + 1;
+};
+
 export function splitContents(contents : string): [string, string]{
     return [contents.slice(0,contents.length / 2), contents.slice(contents.length / 2)];
 };
@@ -9,10 +15,14 @@ export function findInBoth(compartment1:string, compartment2:string): string[]{
     return [...new Set([...compartment1].filter(item => set.has(item)))];
 }
 
-export function part1(inputfile: string) : string[][]{
+export function part1(inputfile: string) : [string,number][]{
     return readFileSync(inputfile, 'utf-8')
         .split(/\r?\n/)
         .filter(line=>line.trim())
         .map(line=>splitContents(line))
-        .map(compartments=>findInBoth(compartments[0], compartments[1]));
+        .map(compartments=>findInBoth(compartments[0], compartments[1]))
+        .flat()
+        .map(item=>[item, prioritise(item)]);
 }
+
+console.log(`AOC 2022 Day 3 result ⭐: ${ part1('src/day3/input.txt').reduce((agg,item) => agg += item[1],0)}`);
--- tests/day3.spec.ts
+++ tests/day3.spec.ts
@@ -1,4 +1,4 @@
-import {splitContents, findInBoth, part1} from '../src/day3/day3';
+import {splitContents, findInBoth, prioritise, part1} from '../src/day3/day3';
 
 const sampleContents = 'vJrwpWtwJgWrhcsFMMfFFhFp';
 
@@ -17,9 +17,15 @@ describe('Day 3 findInBoth', () => {
     });
 });
 
+describe('Day 3 prioritise', () => {
+    it('should assign 16 to p', () => {
+        expect(prioritise('p')).toBe(16);
+    });
+});
+
 describe('Day 3 part 1', () => {
     it ('should find the single common item in each rucksack', () => {
         let repeatedItems = part1('src/day3/testinput.txt');
-        expect(repeatedItems).toStrictEqual([['p'], ['L'], ['P'], ['v'], ['t'], ['s']])
+        expect(repeatedItems).toStrictEqual([['p', 16], ['L', 38], ['P', 42],['v', 22], ['t', 20],['s', 19]]);
     });
 });
