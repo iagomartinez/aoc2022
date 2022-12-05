776b0564d6fb421bf69d00ca21dae102645b5083
Iago Martínez <iago@fundapps.co>
2022-12-05 00:36:35 +0000

    Day 4 ⭐⭐ + Section type to make code more readable :recycle:

--- src/day4/day4.ts
+++ src/day4/day4.ts
@@ -1,29 +1,54 @@
 import { readFileSync } from "fs";
 
-export function pairContains(
-  section1: [number, number],
-  section2: [number, number]
-): boolean {
+export type Section = {
+  start: number;
+  end: number;
+};
+
+export function pairContains(section1: Section, section2: Section): boolean {
   const sorted = [section1, section2].sort((s1, s2) => {
-    return s1[0] - s2[0] || -(s1[1] - s2[1]);
+    // Sort by start position, then check that the end of the resulting first section overlaps the second
+    // Sort sections by start ascending, then end descending - to catch the case where they start on same section
+    // e.g. 3-7,3-6
+    return s1.start - s2.start || -(s1.end - s2.end);
   });
-
-  return sorted[0][1] >= sorted[1][1];
+  return sorted[0].end >= sorted[1].end;
 }
 
-function parseSection(section: string): [number, number] {
+export function pairOverlaps(section1: Section, section2: Section): boolean {
+  const sorted = [section1, section2].sort((s1, s2) => {
+    return s1.start - s2.start || -(s1.end - s2.end);
+  });
+  // Same sort as in overlap case ^; but check that the second start is <= the first end
+  // e.g. 3-7, 7-9
+  return sorted[1].start <= sorted[0].end;
+}
+
+function parseSection(section: string): Section {
   const [sec1, sec2] = [...section.split("-")];
-  return [+sec1, +sec2];
+  return { start: +sec1, end: +sec2 };
 }
 
-export function part1(inputfile: string): number {
+function calculate(
+  inputfile: string,
+  overlapFn: (section1: Section, section2: Section) => boolean
+): number {
   return readFileSync(inputfile, "utf-8")
     .split(/\r?\n/)
     .filter((line) => line.trim())
     .map((line) => line.split(","))
     .map((sections) => [parseSection(sections[0]), parseSection(sections[1])])
-    .map(([sec1, sec2]) => pairContains(sec1, sec2))
+    .map(([sec1, sec2]) => overlapFn(sec1, sec2))
     .filter((isContained) => isContained).length;
 }
 
+export function part1(inputfile: string): number {
+  return calculate(inputfile, pairContains);
+}
+
+export function part2(inputfile: string): number {
+  return calculate(inputfile, pairOverlaps);
+}
+
 console.log(`AOC 2022 Day 4 result ⭐: ${part1("src/day4/input.txt")}`);
+console.log(`AOC 2022 Day 4 result ⭐⭐: ${part2("src/day4/input.txt")}`);
--- tests/day4.spec.ts
+++ tests/day4.spec.ts
@@ -1,22 +1,28 @@
-import { pairContains, part1 } from "../src/day4/day4";
+import {
+  pairContains,
+  part1,
+  part2,
+  pairOverlaps,
+  Section,
+} from "../src/day4/day4";
 
 describe("Day 4 pairContains function", () => {
   it("should identify a clear overlapping section", () => {
-    const section1: [number, number] = [2, 8];
-    const section2: [number, number] = [3, 7];
+    const section1: Section = { start: 2, end: 8 };
+    const section2: Section = { start: 3, end: 7 };
     expect(pairContains(section1, section2)).toBeTruthy();
   });
 
   test.each`
-    section1  | section2  | result
-    ${[6, 6]} | ${[4, 6]} | ${true}
-    ${[2, 3]} | ${[4, 8]} | ${false}
-    ${[2, 6]} | ${[4, 8]} | ${false}
-    ${[2, 6]} | ${[2, 8]} | ${true}
-    ${[1, 1]} | ${[1, 1]} | ${true}
-    ${[1, 3]} | ${[1, 3]} | ${true}
+    section1                | section2                | result
+    ${{ start: 6, end: 6 }} | ${{ start: 4, end: 6 }} | ${true}
+    ${{ start: 2, end: 3 }} | ${{ start: 4, end: 8 }} | ${false}
+    ${{ start: 2, end: 6 }} | ${{ start: 4, end: 8 }} | ${false}
+    ${{ start: 2, end: 6 }} | ${{ start: 2, end: 8 }} | ${true}
+    ${{ start: 1, end: 1 }} | ${{ start: 1, end: 1 }} | ${true}
+    ${{ start: 1, end: 3 }} | ${{ start: 1, end: 3 }} | ${true}
   `(
-    "calculate($play, $response) should return $result",
+    "pairContains($section1, $section2) should return $result",
     ({ section1, section2, result }) => {
       expect(pairContains(section1, section2)).toEqual(result);
     }
@@ -26,5 +32,21 @@ describe("Day 4 pairContains function", () => {
 describe("Day 4 part 1", () => {
   it("should find 2 overlapping pairs in test input", () => {
     expect(part1("src/day4/testinput.txt")).toEqual(2);
+  });
+});
+
+describe("Day 4 part 2", () => {
+  test.each`
+    section1                | section2                | result
+    ${{ start: 5, end: 7 }} | ${{ start: 7, end: 9 }} | ${true}
+  `(
+    "pairOverlaps($section1, $section2) should return $result",
+    ({ section1, section2, result }) => {
+      expect(pairOverlaps(section1, section2)).toEqual(result);
+    }
+  );
+
+  it("should find 4 overlapping pairs in test input", () => {
+    expect(part2("src/day4/testinput.txt")).toEqual(4);
   });
 });
