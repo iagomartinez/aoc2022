8d8278548dfa5923f5fd77da3829d0c34e7b9c85
Iago Martínez <iago@fundapps.co>
2022-12-06 23:39:44 +0000

    Day 6 ⭐⭐ :sparkles:

--- tests/day6/day6.spec.ts
+++ tests/day6/day6.spec.ts
@@ -9,7 +9,21 @@ describe("Part 1", () => {
       ${"nznrnfrfntjfmvfwmzdfjlvtqnbhcprsg"} | ${10}
       ${"zcfzfwzzqfrljwzlrfnpqdbhtmscgvjw"}  | ${11}
     `("scan($line) found in $length characters", ({ line, length }) => {
-      expect(scan(line).length).toBe(length);
+      expect(scan(4, line).length).toBe(length);
+    });
+  });
+});
+describe("Part 2", () => {
+  describe("first marker", () => {
+    test.each`
+      line                                   | length
+      ${"mjqjpqmgbljsphdztnvjfqwrcgsmlb"}    | ${19}
+      ${"bvwbjplbgvbhsrlpgdmjqwftvncz"}      | ${23}
+      ${"nppdvjthqldpwncqszvftbrmjlhg"}      | ${23}
+      ${"nznrnfrfntjfmvfwmzdfjlvtqnbhcprsg"} | ${29}
+      ${"zcfzfwzzqfrljwzlrfnpqdbhtmscgvjw"}  | ${26}
+    `("scan($line) found in $length characters", ({ line, length }) => {
+      expect(scan(14, line).length).toBe(length);
     });
   });
 });
