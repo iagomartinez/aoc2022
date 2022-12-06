91fe2775d5b306531bb353a5d7e1b232e2378a64
Iago Martínez <iago@fundapps.co>
2022-12-06 21:15:49 +0000

    Day 5: Start the move method

--- src/day5/day5.ts
+++ src/day5/day5.ts
@@ -2,9 +2,37 @@
 //  parse crates based on fixed width (3 letters and a space separator)
 //  push them onto arrays
 //  then reverse each array
-//  use pop and push for the movements
+//  use push and shift for the movements
 
 import { readFileSync } from "fs";
+
+export class Stack {
+  stacks: (string | null)[][] = [];
+  build(row: (string | null)[]) {
+    row.forEach((elem, i) => {
+      if (this.stacks[i] === undefined) {
+        console.log(`adding new stack ${i}`);
+        this.stacks.push([]);
+      }
+      if (elem) {
+        console.log(`adding new ${elem} to stack ${i}`);
+        this.stacks[i].push(elem);
+      }
+    });
+  }
+  move(crates: number, from: number, to: number) {
+    throw "not implemented";
+  }
+  get() {
+    return this.stacks;
+  }
+}
+
+export function parseMove(line: string): any {
+  const r =
+    /move (?<move>\d{1,})\sfrom\s(?<from>\d{1,})\sto\s(?<to>\d{1,})/.exec(line);
+  return r ? r.groups : undefined;
+}
 
 export function parseLine(line: string): (string | null)[] {
   let x = 0;
@@ -18,30 +46,27 @@ export function parseLine(line: string): (string | null)[] {
   return stacks;
 }
 
-export function buildStacks(inputfile: string): (string | null)[][] {
+export function rearrange(inputfile: string): Stack {
   const lines = readFileSync(inputfile, "utf-8").split(/\r?\n/);
-  const regex = /\d{1,}\s/g;
+  const countsregex = /$\d{1,}\s/g;
+  const crateregex = /\[[A-Z]/g;
+  const moveregex = /move\s\d{1,}\sfrom\s\d{1,}\sto\s\d{1,}/;
 
-  let line = "";
-  const stacks: (string | null)[][] = [];
-  for (let row = 0; row < lines.length; row++) {
-    line = lines[row];
-    if (line.match(regex)) {
-      console.log("numbers line found");
-      break;
+  const stacks = new Stack();
+  const linecount = lines.length;
+  for (let i = 0; i < linecount; i++) {
+    const line = lines.shift();
+    if (line) {
+      if (line.match(countsregex)) {
+        console.log("numbers line found");
+        continue;
+      } else if (line.match(crateregex)) {
+        stacks.build(parseLine(line));
+      } else if (line.match(moveregex)) {
+        const groups = parseMove(line);
+        //console.log(`move ${groups.move}`);
+      }
     }
-
-    parseLine(line).forEach((elem, i) => {
-      if (stacks[i] === undefined) {
-        console.log(`adding new stack ${i}`);
-        stacks.push([]);
-      }
-      if (elem) {
-        console.log(`adding new ${elem} to stack ${i}`);
-        stacks[i].push(elem);
-      }
-    });
-    console.log(`stacks: ${stacks} after line ${line}; ${line.match(regex)}`);
   }
 
   return stacks;
--- tests/day5/day5.spec.ts
+++ tests/day5/day5.spec.ts
@@ -1,6 +1,6 @@
-import { parseLine, buildStacks } from "../../src/day5/day5";
+import { parseLine, rearrange, parseMove, Stack } from "../../src/day5/day5";
 
-describe("Day 5 part s", () => {
+describe("Day 5 part 1", () => {
   describe("container parsing", () => {
     test.each`
       line             | stacks
@@ -11,18 +11,49 @@ describe("Day 5 part s", () => {
       expect(parseLine(line)).toStrictEqual(stacks);
     });
 
-    it("builds stacks", () => {
-      expect(buildStacks("tests/day5/testinput.txt")).toStrictEqual([
-        ["N", "Z"],
-        ["D", "C", "M"],
-        ["P"],
-      ]);
-    });
+    // it("builds stacks", () => {
+    //   expect(buildStacks("tests/day5/testinput.txt")).toStrictEqual([
+    //     ["N", "Z"],
+    //     ["D", "C", "M"],
+    //     ["P"],
+    //   ]);
+    // });
 
     it("can identify the last line with regex", () => {
       const regex = /\d{1,}\s/g;
       expect("    [D]    ".match(regex)).toBeNull();
       expect(" 1   2   3 ".match(regex)).toBeTruthy();
+    });
+  });
+
+  describe("instruction parsing", () => {
+    it("can extract moves with regex", () => {
+      const input = "move 1 from 2 to 1";
+
+      expect(parseMove(input)).toMatchObject({
+        move: "1",
+        from: "2",
+        to: "1",
+      });
+    });
+  });
+
+  describe("rearrange function", () => {
+    it("returns stack object", () => {
+      const stack = rearrange("tests/day5/testinput.txt");
+      expect(stack).toBeTruthy();
+    });
+  });
+
+  describe("Stack", () => {
+    it("moves crates", () => {
+      const stack = new Stack();
+      const lines = ["    [D]    ", "[N] [C]    ", "[Z] [M] [P]"];
+      lines.map((line) => parseLine(line)).forEach((line) => stack.build(line));
+      const move = parseMove("move 1 from 2 to 1");
+      stack.move(move.move, move.from, move.to);
+      const stacks = [...stack.stacks];
+      expect(stacks[0]).toStrictEqual(["D", "N", "Z"]);
     });
   });
 });
