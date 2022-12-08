8b6074885b6423caf518c1edde24d1fd69dbe7e5
Iago Martínez <iago@fundapps.co>
2022-12-08 01:21:57 +0000

    Parse input file to build file system

--- tests/day7/day7.spec.ts
+++ tests/day7/day7.spec.ts
@@ -10,7 +10,34 @@
 //  cd: moves pointer
 //  ls: creates folder structure; populates folder lookup
 
-//import { matches } from "z";
+import { readFileSync } from "fs";
+
+interface F {
+  file: string;
+  size: number;
+}
+
+interface D {
+  parent?: D;
+  dir: string;
+  size?: number;
+}
+
+class FS {
+  _pwd = "";
+  directories = new Map<string, (F | D)[]>();
+  set pwd(pwd: string) {
+    this._pwd = pwd;
+    console.log(`set pwd ${pwd}`);
+  }
+  add(file: F | D) {
+    if (!this.directories.has(this._pwd)) {
+      this.directories.set(this._pwd, []);
+    }
+    this.directories.get(this._pwd)?.push(file);
+    console.log(`added file ${file}`);
+  }
+}
 
 class CommandParser {
   cache: any[];
@@ -18,23 +45,31 @@ class CommandParser {
   ls = /\$\sls/;
   dir = /dir\s(?<dir>.*)/;
   file = /(?<size>\d{1,})\s(?<file>[\w\.]*)/;
+  fs = new FS();
   constructor() {
     this.cache = [];
   }
   parse(line: string) {
     let m: RegExpExecArray | null;
-    if ((m = this.cd.exec(line))) {
-      console.log(`parsed cd ${m?.groups?.target}`);
-    } else if ((m = this.ls.exec(line))) {
-      console.log(`parsed ls`);
-    } else if ((m = this.file.exec(line))) {
-      console.log(`parsed file ${m?.groups?.size}, ${m?.groups?.file}`);
+    if ((m = this.cd.exec(line)) && m?.groups?.target) {
+      this.fs.pwd = m?.groups?.target;
+    }
+    //else if ((m = this.ls.exec(line))) {
+    //console.log(`parsed ls`);
+    //}
+    else if ((m = this.file.exec(line))) {
+      if (m.groups?.file && m.groups?.size) {
+        this.fs.add({ file: m.groups?.file, size: +m.groups?.size });
+      }
     } else if ((m = this.dir.exec(line))) {
-      console.log(`parsed dir ${m?.groups?.dir}`);
+      if (m.groups?.dir) {
+        this.fs.add({ dir: m.groups?.dir });
+      }
+      //console.log(`parsed dir ${m?.groups?.dir}`);
     }
   }
   getFs() {
-    return this.cache;
+    return this.fs;
   }
 }
 
@@ -54,12 +89,13 @@ describe("command parsing", () => {
 
   describe("CommandParser", () => {
     const parser = new CommandParser();
-    parser.parse("$ cd /");
-    parser.parse("$ ls");
-    parser.parse("dir a");
-    parser.parse("14848514 b.txt");
 
-    expect(parser.getFs()).toBeTruthy();
+    readFileSync("tests/day7/testinput.txt", "utf-8")
+      .split(/\r?\n/)
+      .forEach((line) => parser.parse(line));
+
+    console.log(parser.getFs().directories);
+    expect(parser.getFs().directories).toBeTruthy();
   });
 
   describe("match file and size", () => {
